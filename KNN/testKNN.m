ee508ab7d521e70256b2107bd003bd2065447039
masonrware <86129546+masonrware@users.noreply.github.com>
2022-03-23 09:41:38 -0400

    start hw5 and update hw5 with mat code

---
+++ KNN/testKNN.m
@@ -0,0 +1,62 @@
+load KNN_Data;
+plot( data1(:,1), data1(:,2), 'r.', data2(:,1), data2(:,2), 'b.', test(:,1), test(:,2), 'k^', 'MarkerSize', 10 );
+
+%%
+knnModel = ClassificationKNN.fit( [data1; data2], [ones(size(data1,1),1); 2*ones(size(data2,1),1)] );
+y = predict(knnModel, test);
+figure;
+plot( data1(:,1), data1(:,2), 'r.', data2(:,1), data2(:,2), 'b.', test(:,1), test(:,2), 'k^', 'MarkerSize', 10 );
+hold on;
+if y == 1
+    plot( test(:,1), test(:,2), 'r^' );
+elseif y == 2
+    plot( test(:,1), test(:,2), 'b^' );
+end
+title( ['K-NN: k = ', num2str(knnModel.NumNeighbors), ', Distance = ', knnModel.Distance] );
+hold off;
+
+%% Change the number of neighbors
+knnModel.NumNeighbors = 3;
+y = predict(knnModel, test);
+figure;
+plot( data1(:,1), data1(:,2), 'r.', data2(:,1), data2(:,2), 'b.', test(:,1), test(:,2), 'k^', 'MarkerSize', 10 );
+hold on;
+if y == 1
+    plot( test(:,1), test(:,2), 'r^' );
+elseif y == 2
+    plot( test(:,1), test(:,2), 'b^' );
+end
+title( ['K-NN: k = ', num2str(knnModel.NumNeighbors), ', Distance = ', knnModel.Distance] );
+hold off;
+
+%% Use 'cosine' distance
+knnModel = ClassificationKNN.fit( [data1; data2], [ones(size(data1,1),1); 2*ones(size(data2,1),1)], ...
+                            'NSMethod','exhaustive', 'Distance','cosine');
+knnModel.NumNeighbors = 1;
+y = predict(knnModel, test);
+figure;
+plot( data1(:,1), data1(:,2), 'r.', data2(:,1), data2(:,2), 'b.', test(:,1), test(:,2), 'k^', 'MarkerSize', 10 );
+hold on;
+if y == 1
+    plot( test(:,1), test(:,2), 'r^' );
+elseif y == 2
+    plot( test(:,1), test(:,2), 'b^' );
+end
+title( ['K-NN: k = ', num2str(knnModel.NumNeighbors), ', Distance = ', knnModel.Distance] );
+hold off;
+
+%% 
+knnModel = ClassificationKNN.fit( [data1; data2], [ones(size(data1,1),1); 2*ones(size(data2,1),1)] );
+knnModel.NumNeighbors = 10;  % 3 , 10
+knnModel.DistanceWeight = 'inverse';
+y = predict(knnModel, test);
+figure;
+plot( data1(:,1), data1(:,2), 'r.', data2(:,1), data2(:,2), 'b.', test(:,1), test(:,2), 'k^', 'MarkerSize', 10 );
+hold on;
+if y == 1
+    plot( test(:,1), test(:,2), 'r^' );
+elseif y == 2
+    plot( test(:,1), test(:,2), 'b^' );
+end
+title( ['K-NN: k = ', num2str(knnModel.NumNeighbors), ', Distance = ', knnModel.Distance, ', DistanceWeight = ', knnModel.DistanceWeight] );
+hold off;
