4d06654fbae00e160453a8a71d37b678168e338f
masonrware <86129546+masonrware@users.noreply.github.com>
2022-04-10 23:10:32 -0400

    updates

--- termProject/main.py
+++ termProject/main.py
@@ -1,23 +1,71 @@
-# EXAMPLE CODE:::
-# # Import the following modules
-# import pytesseract
-# from PIL import Image
-# import os
-# import pywhatkit as kit
+# Importing all necessary libraries
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import backend as K
 
-# # Change the directory to the
-# # location where image is present
-# os.chdir(r"C:\Users\Dell\Downloads")
+img_width, img_height = 456, 160
 
-# # Set the Path of Tesseract
-# pytesseract.pytesseract.tesseract_cmd = r"C:\Users\Dell\AppData\/
-# Local\Tesseract-OCR\tesseract.exe"
+train_data_dir = 'data'
+# validation_data_dir = 'v_data/test'
+nb_train_samples = 860
+# nb_validation_samples = 100
+epochs = 10
+batch_size = 16
 
-# # Load the Image
-# img = Image.open("GFG.png")
+if K.image_data_format() == 'channels_first':
+	input_shape = (3, img_width, img_height)
+else:
+	input_shape = (img_width, img_height, 3)
 
-# # Convert Image to Text
-# text = pytesseract.image_to_string(img)
+model = Sequential()
+model.add(Conv2D(32, (2, 2), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
-# # Convert Text to Hand Written Text
-# kit.text_to_handwriting(text, rgb=[0, 0, 250])
+model.add(Conv2D(32, (2, 2)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (2, 2)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+			optimizer='rmsprop',
+			metrics=['accuracy'])
+
+train_datagen = ImageDataGenerator(
+	rescale=1. / 255,
+	shear_range=0.2,
+	zoom_range=0.2,
+	horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow_from_directory(
+	train_data_dir,
+	target_size=(img_width, img_height),
+	batch_size=batch_size,
+	class_mode='binary')
+
+validation_generator = test_datagen.flow_from_directory(
+	validation_data_dir,
+	target_size=(img_width, img_height),
+	batch_size=batch_size,
+	class_mode='binary')
+
+model.fit_generator(
+	train_generator,
+	steps_per_epoch=nb_train_samples // batch_size,
+	epochs=epochs,
+	validation_data=validation_generator,
+	validation_steps=nb_validation_samples // batch_size)
