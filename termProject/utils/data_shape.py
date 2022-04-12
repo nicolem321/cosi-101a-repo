4cf00849360332e3119ae2128d12ea8a809f53d5
masonrware <86129546+masonrware@users.noreply.github.com>
2022-04-11 20:39:30 -0400

    hierarchy adjustment

--- termProject/utils/data_shape.py
+++ termProject/utils/data_shape.py
@@ -1,7 +1,13 @@
 import pandas as pd
 
+
+#### REDO DATA TO HAVE NEW HIERARCHY
+
 in_file_path = 'termProject/data/train/labels.txt'
 out_file_path = 'termProject/data/labels.csv'
 df = pd.read_csv(in_file_path, delimiter = '\t')
 df.to_csv(out_file_path, index = None)
 
+
+
+#### CREATE DATAFRAMES FOR EACH CLASS
