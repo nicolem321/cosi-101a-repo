d66805768901f4fafd8fe885b5077c68c470aec5
masonrware <86129546+masonrware@users.noreply.github.com>
2022-04-17 10:03:10 -0700

    progress on hw6

--- HW6/main.py
+++ HW6/main.py
@@ -39,7 +39,7 @@ y_test = idx2numpy.convert_from_file(test_label_file)
 @timer
 # load train and test dataset
 def load_dataset() -> Tuple:
-	# reshape dataset to have a single channel
+    # reshape dataset to have a single channel
     trainX = X_train.reshape((X_train.shape[0], 28, 28, 1))
     testX = X_train.reshape((X_train.shape[0], 28, 28, 1))
     
@@ -52,36 +52,34 @@ def load_dataset() -> Tuple:
 # scale pixels
 def prep_pixels(train, test):
 	# convert from integers to floats
-	train_norm = train.astype('float32')
-	test_norm = test.astype('float32')
-	# normalize to range 0-1
-	train_norm = train_norm / 255.0
-	test_norm = test_norm / 255.0
-	# return normalized images
-	return train_norm, test_norm
+    train_norm = train.astype('float32')
+    test_norm = test.astype('float32')
+    # normalize to range 0-1
+    train_norm = train_norm / 255.0
+    test_norm = test_norm / 255.0
+    # return normalized images
+    return train_norm, test_norm
 
 @timer
 # define cnn model
 def define_model():
-	model = Sequential()
-	model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(28, 28, 1)))
-	model.add(MaxPooling2D((2, 2)))
-	model.add(Flatten())
-	model.add(Dense(100, activation='relu', kernel_initializer='he_uniform'))
-	model.add(Dense(10, activation='softmax'))
-	# compile model
-	opt = SGD(learning_rate=0.01, momentum=0.9)
-	model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
-	return model
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(28, 28, 1)))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Flatten())
+    model.add(Dense(100, activation='relu', kernel_initializer='he_uniform'))
+    model.add(Dense(10, activation='softmax'))
+    # compile model
+    opt = SGD(learning_rate=0.01, momentum=0.9)
+    model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
 
 @timer
 # evaluate a model using k-fold cross-validation
 def evaluate_model(dataX, dataY, n_folds=5):
-    print('model start')
     scores, histories = list(), list()
     # prepare cross validation
     kfold = KFold(n_folds, shuffle=True, random_state=1)
-    print(kfold)
     # enumerate splits
     for train_ix, test_ix in kfold.split(dataX):
         # define model
@@ -90,14 +88,12 @@ def evaluate_model(dataX, dataY, n_folds=5):
         trainX, trainY, testX, testY = dataX[train_ix], dataY[train_ix], dataX[test_ix], dataY[test_ix]
         # fit model
         history = model.fit(trainX, trainY, epochs=10, batch_size=32, validation_data=(testX, testY), verbose=0)
-        print('got history')
         # evaluate model
         _, acc = model.evaluate(testX, testY, verbose=0)
         print('> %.3f' % (acc * 100.0))
         # stores scores
         scores.append(acc)
         histories.append(history)
-        print('iter')
 
     return scores, histories
 
@@ -132,12 +128,10 @@ def run_test_harness():
     #load data set
     trainX, trainY, testX, testY = load_dataset()
     # prepare pixel data
-    print('data loaded!!')
     trainX, testX = prep_pixels(trainX, testX)
     print('pixels prepped!!')
     # evaluate model
     scores, histories = evaluate_model(trainX, trainY)
-    print('model run!!')
     # learning curves
     summarize_diagnostics(histories)
 	# summarize estimated performance
