80cadf2a83ddeae263777dff7eb26320da241e44
masonware-ctrl <86129546+masonrware@users.noreply.github.com>
2022-04-19 16:55:48 -0700

    Update main.py

--- termProject/main.py
+++ termProject/main.py
@@ -17,11 +17,12 @@ from typing import Tuple
 from utils import timer
 
 # import idx2numpy                                            # type: ignore
-# import numpy as np                                          # type: ignore
+import numpy as np                                          # type: ignore
 # from mlxtend.data import loadlocal_mnist                    # type: ignore
 # from numpy import mean                                      # type: ignore
 # from numpy import std                                       # type: ignore
-# from matplotlib import pyplot as plt                        # type: ignore
+from matplotlib import pyplot as plt                        # type: ignore
+import matplotlib.image as mpimg                            # type: ignore
 # from sklearn.model_selection import KFold                   # type: ignore
 # from tensorflow.keras.utils import to_categorical           # type: ignore
 # from tensorflow.keras.models import Sequential              # type: ignore
@@ -31,49 +32,6 @@ from utils import timer
 # from tensorflow.keras.layers import Flatten                 # type: ignore
 # from tensorflow.keras.optimizers import SGD                 # type: ignore
 
-#############################################################################
-#############################################################################
-from PIL import Image, ImageFilter                          # type: ignore
-
-#? output to the same file every time?
-# I wouldn't know how to check for new lines althought writing might auto
-# that for me
-
-def imageprepare(argv):
-    """
-    This function returns the pixel values.
-    The imput is a png file location.
-    """
-    im = Image.open(argv).convert('L')
-    width = float(im.size[0])
-    height = float(im.size[1])
-    newImage = Image.new('L', (28, 28), (255))  # creates white canvas of 28x28 pixels
-
-    if width > height:  # check which dimension is bigger
-        # Width is bigger. Width becomes 20 pixels.
-        nheight = int(round((20.0 / width * height), 0))  # resize height according to ratio width
-        if (nheight == 0):  # rare case but minimum is 1 pixel
-            nheight = 1
-            # resize and sharpen
-        img = im.resize((20, nheight), Image.ANTIALIAS).filter(ImageFilter.SHARPEN)
-        wtop = int(round(((28 - nheight) / 2), 0))  # calculate horizontal position
-        newImage.paste(img, (4, wtop))  # paste resized image on white canvas
-    else:
-        # Height is bigger. Heigth becomes 20 pixels.
-        nwidth = int(round((20.0 / height * width), 0))  # resize width according to ratio height
-        if (nwidth == 0):  # rare case but minimum is 1 pixel
-            nwidth = 1
-        # resize and sharpen
-        img = im.resize((nwidth, 20), Image.ANTIALIAS).filter(ImageFilter.SHARPEN)
-        wleft = int(round(((28 - nwidth) / 2), 0))  # caculate vertical pozition
-        newImage.paste(img, (wleft, 4))  # paste resized image on white canvas
-
-    tv = list(newImage.getdata())  # get pixel values
-
-    # normalize pixels to 0 and 1. 0 is pure white, 1 is pure black.
-    tva = [(255 - x) * 1.0 / 255.0 for x in tv]
-    # print(tva)
-    return tva
 
 from utils.data import DataPy as dpy
 
@@ -82,12 +40,21 @@ dpy.read_data(dpy, dir_str)
 data_df = dpy.get_df(dpy)
 train = list()
 
+
 for index, row in data_df.iterrows():
-    train.append(imageprepare(row['image_name']))
+    train.append(mpimg.imread(row['image_name']))
+
+# visualize first 9 images
+for i in range(9): 
+    plt.subplot(330 + 1 + i) 
+    plt.imshow(train[i], cmap=plt.get_cmap('gray'))
+plt.show()
+
+arr = np.array(train)
+print(arr)
 
 #TODO
-#write this list to a txt file like the ubytes - see if there's a special process
-
+#can I now just start working with train?
 
 #############################################################################
 #############################################################################
